983f7b3d9cab200873b0b62121466b85e7c74bc7
JorgeLNJunior <jorgelnjunior@gmail.com>
2022-11-24 11:00:17 -0300

    chore: commit inicial

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-layout>
+    <v-app-bar color="primary">
+      <v-app-bar-title>Proxy Printer</v-app-bar-title>
+    </v-app-bar>
+    <v-main></v-main>
+  </v-layout>
+</template>
