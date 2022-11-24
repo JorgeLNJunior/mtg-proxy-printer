9c55e0541d76f9ea563df87ffc6108d8bc68ec61
JorgeLNJunior <jorgelnjunior@gmail.com>
2022-11-24 14:22:23 -0300

    feat: criar layout básico

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,53 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { deckExample } from './texts/texts'
+
+const deck = ref<string>('')
+
+function clear(): void {
+  deck.value = ''
+}
+</script>
 
 <template>
   <v-layout>
     <v-app-bar color="primary">
-      <v-app-bar-title>Proxy Printer</v-app-bar-title>
+      <v-app-bar-title class="select-none">Proxy Printer</v-app-bar-title>
     </v-app-bar>
-    <v-main></v-main>
+    <v-main>
+      <v-container fluid>
+        <v-row justify="center" class="ma-4">
+          <v-card class="text-center pa-2" elevation="2" rounded>
+            <v-card-title class="select-none">Deck</v-card-title>
+            <v-card-subtitle class="select-none">
+              Insira o deck no formato
+              <a
+                class="text-decoration-none"
+                href="https://www.mtggoldfish.com/deck/arena_download/5211385"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <b>padrão</b>
+              </a>
+              do Magic Arena. As cartas
+              <b>devem</b>
+              estar em Inglês.
+            </v-card-subtitle>
+            <v-card-text>
+              <v-textarea
+                v-model="deck"
+                :no-resize="true"
+                :auto-grow="true"
+                :placeholder="deckExample"
+              ></v-textarea>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="primary" variant="flat">Enviar</v-btn>
+              <v-btn color="accent" @click="clear()">Limpar</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-row>
+      </v-container>
+    </v-main>
   </v-layout>
 </template>
---
+++ src/texts/texts.ts
@@ -0,0 +1,7 @@
+export const deckExample = `
+4 Haughty Djinn
+3 Slip Out the Back
+4 Tolarian Terror
+4 Consider
+4 Thirst for Discovery
+`.trim()
