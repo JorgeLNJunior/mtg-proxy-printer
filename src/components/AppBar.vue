f742403590adad4beb6812f0d5c87165682862d0
JorgeLNJunior <jorgelnjunior@gmail.com>
2022-11-24 14:51:57 -0300

    feat: implementar o light/dark mode

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,7 +1,18 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+
+function toggleTheme() {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+</script>
 
 <template>
   <v-app-bar color="primary">
     <v-app-bar-title class="select-none">Proxy Printer</v-app-bar-title>
+    <v-btn icon @click="toggleTheme()">
+      <v-icon>mdi-theme-light-dark</v-icon>
+    </v-btn>
   </v-app-bar>
 </template>
