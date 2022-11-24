1d1d4e0b8b0e16b6d7ae0e9af0a0d0f2feb7d6b9
JorgeLNJunior <jorgelnjunior@gmail.com>
2022-11-24 15:00:39 -0300

    feat: adicionar o link para o github

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -11,6 +11,14 @@ function toggleTheme() {
 <template>
   <v-app-bar color="primary">
     <v-app-bar-title class="select-none">Proxy Printer</v-app-bar-title>
+    <v-btn
+      icon
+      href="https://github.com/JorgeLNJunior/mtg-proxy-printer"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <v-icon>mdi-github</v-icon>
+    </v-btn>
     <v-btn icon @click="toggleTheme()">
       <v-icon>mdi-theme-light-dark</v-icon>
     </v-btn>
